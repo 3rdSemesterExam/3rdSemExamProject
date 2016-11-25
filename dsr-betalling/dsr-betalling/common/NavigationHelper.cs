8d5ef7a0c300215be710bfb0b8949430378ce720
jwin74 <jasp0359@edu.easj.dk>
2016-11-25 09:27:46 +0100

    DSR-Betalling: Added Helper Classes to Common

---
+++ dsr-betalling/dsr-betalling/common/NavigationHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using dsr_betalling.handler;
+
+namespace dsr_betalling.common
+{
+    class NavigationHelper
+    {
+        /// <summary>
+        /// Denne klasse gør at man kan navigaere fra C# koden i stedet for Xaml koden
+        /// </summary>
+        private static Frame _frame;
+
+        public static void navigate(Type page)
+        {
+            try
+            {
+                _frame = (Window.Current.Content as Frame);
+                _frame?.Navigate(page); // Hvis _frame IKKE er null
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.ShowExceptionError(ex.Message);
+            }
+        }
+    }
+}
