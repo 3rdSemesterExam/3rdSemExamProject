606836994b396090a1dd4ad05f5668b2096ef2c5
Allando <alla2634@edu.easj.dk>
2016-12-02 10:17:45 +0100

    MainPage RE: added

--- dsr-betalling/dsr-betalling/view/MainPage.xaml.cs
+++ dsr-betalling/dsr-betalling/view/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using dsr_betalling.Common;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,11 @@ namespace dsr_betalling.view
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void LogInButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            NavigationHelper.Navigate(typeof(Sale));
         }
     }
 }
