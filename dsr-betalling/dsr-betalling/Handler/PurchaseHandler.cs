353d80ca66785aea0fc4ffa6634927d22c63d444
jwin74 <jasp0359@edu.easj.dk>
2016-12-05 09:55:31 +0100

    Updated PurchaseHandler with make purchase

--- dsr-betalling/dsr-betalling/Handler/PurchaseHandler.cs
+++ dsr-betalling/dsr-betalling/Handler/PurchaseHandler.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dsr_betalling.Model;
 
 namespace dsr_betalling.Handler
 {
     class PurchaseHandler
     {
+        public bool MakePurchase(List<PurchaseItems> PurchaseItems, string ChipId)
+        {
+            return false;
+        }
     }
 }
