366967a2530ec4621e96f59866b4a5ecd66b13b1
Mads Veelsgaard <mads581b@edu.easj.dk>
2016-12-05 09:26:53 +0100

    INotifyPropertyChanged - VmAcces

--- dsr-betalling/dsr-betalling/ViewModel/vmAccess.cs
+++ dsr-betalling/dsr-betalling/ViewModel/vmAccess.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using dsr_betalling.Annotations;
 
 namespace dsr_betalling.ViewModel
 {
-    class vmAccess
+    class vmAccess : INotifyPropertyChanged
     {
+
+
+
+
+
+        #region MyRegion
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            #endregion
+        }
     }
 }
