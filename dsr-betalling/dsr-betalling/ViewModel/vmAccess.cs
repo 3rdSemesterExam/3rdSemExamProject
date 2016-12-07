e18ccb473409a66cc5c6f0432c90876c868c572a
Allando <alla2634@edu.easj.dk>
2016-12-07 09:38:36 +0100

    vmAccess code added

--- dsr-betalling/dsr-betalling/ViewModel/vmAccess.cs
+++ dsr-betalling/dsr-betalling/ViewModel/vmAccess.cs
@@ -5,16 +5,39 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using dsr_betalling.Annotations;
 
 namespace dsr_betalling.ViewModel
 {
     class vmAccess : INotifyPropertyChanged
     {
-        
+        private string _username;
+        private string _password;
+        public ICommand LogiCommand { get; set; }
 
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(); }
+        }
 
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
 
+        public vmAccess()
+        {
+            
+        }
+
+        //
+        public void Login()
+        {
+            
+        }
 
         #region MyRegion
         public event PropertyChangedEventHandler PropertyChanged;
