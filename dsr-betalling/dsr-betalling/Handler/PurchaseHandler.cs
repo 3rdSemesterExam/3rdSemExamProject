813f52b07ee7f13011bf435a5fd2099d96a3579f
jwin74 <jasp0359@edu.easj.dk>
2016-12-08 10:56:43 +0100

    dsr-betalling: Added MakePurchase to PurchaseHandler. Updated Purchase and PurchaseItem constructors

--- dsr-betalling/dsr-betalling/Handler/PurchaseHandler.cs
+++ dsr-betalling/dsr-betalling/Handler/PurchaseHandler.cs
@@ -1,17 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using dsr_betalling.Common;
 using dsr_betalling.Model;
 
 namespace dsr_betalling.Handler
 {
-    class PurchaseHandler
+    public class PurchaseHandler
     {
-        public bool MakePurchase(List<PurchaseItems> PurchaseItems, string ChipId)
+        public async Task<bool> MakePurchase(List<PurchaseItem> PurchaseItems, string ChipId, float Discount)
         {
-            return false;
+            var result = true;
+
+            // Gather Info
+            var totalPrice = PurchaseItems.Sum(purchaseItem => purchaseItem.Amount * purchaseItem.Price) - Discount;
+            var Account = AccountHandler.GetAccount(ChipHandler.GetAccountIdFromChipId(ChipId)).Result;
+            
+            // Post Purchase
+            var PurchaseId = int.Parse(await Facade.PostScalarAsync(new Purchase(Account.Id, Authorization.UserId, totalPrice, DateTime.Now)));
+
+            // Post PurchaseItems
+            foreach (var purchaseItem in PurchaseItems)
+            {
+                purchaseItem.FK_Purchase = PurchaseId;
+                if (!await Facade.PostAsync(purchaseItem))
+                    result = false;
+            }
+
+            // Verify Result
+            if (!result) return false;
+
+            // Withdraw Funds, Update Account
+            Account.WithdrawFunds(totalPrice);
+            result = await AccountHandler.UpdateAccount(Account);
+
+            return result;
         }
     }
 }
---
+++ dsr-betalling/dsr-betalling/model/PurchaseItem.cs
@@ -0,0 +1,29 @@
+﻿using dsr_betalling.Interface;
+
+namespace dsr_betalling.Model
+{
+    public class PurchaseItem : IWebUri
+    {
+        public int Id { get; set; }
+        public int FK_Purchase { get; set; }
+        public int FK_Product { get; set; }
+        public int Amount { get; set; }
+        public float Price { get; set; }
+        public string ResourceUri { get; }
+        public string VerboseName { get; }
+
+        private PurchaseItem()
+        {
+            ResourceUri = "PurchaseItem";
+            VerboseName = "PurchaseItem";
+        }
+
+        public PurchaseItem(int fkPurchase, int fkProduct, int amount, float price) : this()
+        {
+            FK_Purchase = fkPurchase;
+            FK_Product = fkProduct;
+            Amount = amount;
+            Price = price;
+        }
+    }
+}
