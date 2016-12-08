90097b8198eb8d87e7e0a3a1aa19cf3c5036c2d3
jwin74 <jasp0359@edu.easj.dk>
2016-12-08 11:02:31 +0100

    dsr-betalling Facade: Added PostScalarAsync Method Facade: Minor Update to DoCreateUser Authorization: Added AuthorizationHandler Authorization: Added Authorization DataRepo class

---
+++ dsr-betalling/dsr-betalling/Handler/AuthorizationHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dsr_betalling.Common;
+
+namespace dsr_betalling.Handler
+{
+    public class AuthorizationHandler
+    {
+        public bool DoLogin(string username, string password)
+        {
+            var result = Facade.DoLoginAsync(username, password).Result;
+            Authorization.UpdateUserList();
+            Authorization.SetUserId(username);
+            return result;
+        }
+
+        public bool DoLogout()
+        {
+            var result = Facade.DoLogout();
+            Authorization.Clear();
+            return result;
+        }
+    }
+}
---
+++ dsr-betalling/dsr-betalling/common/Authorization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dsr_betalling.Model;
+
+namespace dsr_betalling.Common
+{
+    public static class Authorization
+    {
+        private static IEnumerable<User> userList;
+
+        public static int UserId { get; private set; }
+        public static string Username { get; private set; }
+
+        public static async void UpdateUserList()
+        {
+            userList = await Facade.GetListAsync(new User());
+        }
+
+        public static void SetUserId(string username)
+        {
+            UserId = userList.FirstOrDefault(user => user.Username == username).Id;
+        }
+
+        public static void SetUsername(int userid)
+        {
+            Username = userList.FirstOrDefault(user => user.Id == userid).Username;
+        }
+
+        public static void Clear()
+        {
+            UserId = -1;
+            Username = null;
+        }
+    }
+}
